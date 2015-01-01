879011cfed3f84631aa68c7b7a825928e5eccfcf
Jay Wengrow <jaywngrw@gmail.com>
2015-01-01 11:38:37 -0600

    automatically create profile after user is created

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,14 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  after_create :create_profile
+
   has_many :photos
   has_one :profile
+
+  private
+
+  def create_profile
+    Profile.create(user_id: self.id)
+  end
 end
